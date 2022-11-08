2ca11afd83e5f992de439112d05717141a566df8
Sazzad Hosen <mdsazzadhosentamim95@gmail.com>
2022-11-08 23:40:27 +0600

    added specific service route

--- index.js
+++ index.js
@@ -21,7 +21,21 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('photographer').collection('pServices');
-        const orderCollection = client.db('photographer').collection('orders');
+        // const orderCollection = client.db('photographer').collection('orders');
+
+        app.get('/pServices', async(req, res) =>{
+            const query ={}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/pServices/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
     }
     finally{
 
