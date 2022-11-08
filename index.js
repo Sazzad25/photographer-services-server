7918536856d2a5202d9e9cee42d7b0d16c217071
Sazzad Hosen <mdsazzadhosentamim95@gmail.com>
2022-11-08 16:03:22 +0600

    conect to mongo db

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mwoallb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req,res) =>{
+    res.send('photographer server is running')
+})
+
+app.listen(port, () =>{
+    console.log(`photographer server running on ${port}`);
+})
